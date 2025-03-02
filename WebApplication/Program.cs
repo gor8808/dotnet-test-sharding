d83a5b0b95a90b13cfa6ad90126eb105ea50e01b
Gor Grigoryan <gor.grigoryan04.24@gmail.com>
2025-03-02 23:01:00 +0400

    Add draft project setup

---
+++ WebApplication/Program.cs
@@ -0,0 +1,51 @@
+namespace WebApplication;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", async () =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        (
+                            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            Random.Shared.Next(-20, 55),
+                            summaries[Random.Shared.Next(summaries.Length)]
+                        ))
+                    .ToArray();
+
+                await Task.Delay(1000);
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+
+        app.Run();
+    }
+}
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
