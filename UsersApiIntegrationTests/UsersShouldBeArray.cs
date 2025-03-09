bd4069aea5d6960829a7bed7ebd785c5b3aa33e6
Gor Grigoryan <gor.grigoryan04.24@gmail.com>
2025-03-09 19:49:16 +0400

    Change test case

--- UsersApiIntegrationTests/UsersShouldBeArray.cs
+++ UsersApiIntegrationTests/UsersShouldBeArray.cs
@@ -12,9 +12,8 @@ public class UsersShouldBeArray
     }
 
     
-    [Theory]
-    [MemberData(nameof(EnumerableRange))]
-    public async Task Users_ShouldBeArray(int number)
+    [Fact]
+    public async Task Users_ShouldBearray_11()
     {
         var response = await _cleint.GetAsync("/users");
         
@@ -25,5 +24,214 @@ public class UsersShouldBeArray
         Assert.True(content.StartsWith("[") && content.EndsWith("]"));
     }
     
-    public static object[][] EnumerableRange = Enumerable.Range(1, 50).Select(i => new object[] { i }).ToArray();
+    [Fact]
+    public async Task Users_ShouldBearray_12()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBearray_13()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBearray_14()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBearray_15()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBearray_16()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBearray_17()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBearray_18()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBearray_19()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBearray_20()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeArray_1()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    
+    [Fact]
+    public async Task Users_ShouldBeArray_2()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeArray_3()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeArray_4()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeArray_5()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeArray_6()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeArray_7()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeArray_8()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeArray_9()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeArray_10()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
 }
--- UsersApiIntegrationTests/UsersShouldBeSuccessStatusCode.cs
+++ UsersApiIntegrationTests/UsersShouldBeSuccessStatusCode.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
 namespace UsersApiIntegrationTests;
 
 public class UsersShouldBeSuccessStatusCode
@@ -9,9 +11,8 @@ public class UsersShouldBeSuccessStatusCode
         _cleint = fixture.Factory.CreateClient();
     }
     
-    [Theory]
-    [MemberData(nameof(EnumerableRange))]
-    public async Task Users_ShouldBeSuccessStatusCode(int number)
+    [Fact]
+    public async Task Users_ShouldBeSuccess_11()
     {
         var response = await _cleint.GetAsync("/users");
         
@@ -19,7 +20,217 @@ public class UsersShouldBeSuccessStatusCode
         
         var content = await response.Content.ReadAsStringAsync();
         Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
     }
     
-    public static object[][] EnumerableRange = Enumerable.Range(1, 50).Select(i => new object[] { i }).ToArray();
+    [Fact]
+    public async Task Users_ShouldBeSuccess_12()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeSuccess_13()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeSuccess_14()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeSuccess_15()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeSuccess_16()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeSuccess_17()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeSuccess_18()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeSuccess_19()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeSuccess_20()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeSuccess_1()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    
+    [Fact]
+    public async Task Users_ShouldBeSuccess_2()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeSuccess_3()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeSuccess_4()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeSuccess_5()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeSuccess_6()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeSuccess_7()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeSuccess_8()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeSuccess_9()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
+    [Fact]
+    public async Task Users_ShouldBeSuccess_10()
+    {
+        var response = await _cleint.GetAsync("/users");
+        
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.False(string.IsNullOrWhiteSpace(content));
+        Assert.True(content.StartsWith("[") && content.EndsWith("]"));
+    }
 }
